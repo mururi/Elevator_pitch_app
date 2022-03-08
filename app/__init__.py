57353125c3c0a390470c84d9422b36607b42ca60
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-08 22:30:41 +0300

    Add welcome email after signup

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+mail = Mail()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -22,6 +24,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     bootstrap.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
+from ..email import mail_message
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
@@ -12,6 +13,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to Elevator Pitch App","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
