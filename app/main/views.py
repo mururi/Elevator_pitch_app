b896d1b70bf37419c75002e2e0a463bda026f24e
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-08 15:36:27 +0300

    Add email column and migrate the db

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from turtle import title
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 # Views
 @main.route('/')
@@ -11,3 +12,7 @@ def index():
     '''
     title = 'Elevator Pitch'
     return render_template('index.html', title = title)
+
+# @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+# @login_required
+# def new_review(id):
