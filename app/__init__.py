ac352278cb5df41f17fe5905f25312e448f382a0
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-08 16:38:24 +0300

    Install and initialize bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
+from ensurepip import bootstrap
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -19,6 +22,7 @@ def create_app(config_name):
     # Initializing flask extensions
     db.init_app(app)
     login_manager.init_app(app)
+    bootstrap.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
