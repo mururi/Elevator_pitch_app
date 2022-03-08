e2a6f8add24fe5339d5ded0214c20d3ff33cbaa9
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-08 18:52:04 +0300

    Add profile and profile update templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
 from turtle import title
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort
+from ..models import User
 from . import main
 from flask_login import login_required
+from .forms import UpdateProfile
+from .. import db
 
 # Views
 @main.route('/')
@@ -12,6 +15,35 @@ def index():
     '''
     title = 'Elevator Pitch'
     return render_template('index.html', title = title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 
 # @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 # @login_required
